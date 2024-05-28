fc12ae96c17063102ea67df8b842e166e4eaa204
mj-linane <19750620+mj-linane@users.noreply.github.com>
2024-05-28 11:42:52 -0400

    chore: Update index.js to render App component in React strict mode

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ import "./styles.css";
 
 import App from "./App";
 
-const root = createRoot(document.getElementById("root"));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement !== null) {
+  const root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error("No element with id 'root' found in the document.");
+}
