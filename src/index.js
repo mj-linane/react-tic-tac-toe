8a2cd65892a8383bda9584ca3f5cba8f16593d22
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2024-05-28 17:33:52 -0400

    chore: Update import statement for App component in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import App from "./App.js";
 
 const rootElement = document.getElementById("root");
 
